fe39d083096f2234f7576aa209c279c0cb6e1640
qiaomengnan16 <285921957@qq.com>
2018-10-15 23:56:46 +0800

    Add files via upload

---
+++ musicPlayer/README.md
@@ -0,0 +1,145 @@
+#本项目实现了一个简单的音乐播放器功能
+具体功能有
+1.播放
+设置Audio对象的音乐src引用,并进行play()事件开始播放
+2.暂停
+调用Audio对象的pause()方法,暂停播放
+3.上一曲
+获得列表中上一个index的src,然后更改Audio对象的音乐src引用
+4.下一曲
+获得列表中下一个index的src,然后更改Audio对象的音乐src引用
+5.循环播放
+使用 % 算法,最后一首播放完毕后从0开始
+6.列表展示
+循环添加li标签展示歌曲
+7.点击列表中的一首歌进行播放
+使用事件代理，点击了列表后,播放选中的歌曲
+8.进度条，点击进度条进行跳跃播放
+获取用户点击进度条的offsetX,除以总宽度,得到进度百分比,设置Audio对象的duration属性
+
+
+核心功能采用了Audio对象，用于音乐的播放暂停等功能
+
+1. audioObject
+ 创建或者获取的audio对象，可通过以下两种方式得到
+
+**方法1:**
+```html
+<audio id="music" src="http://cloud.hunger-valley.com/music/玫瑰.mp3">你的浏览器不支持喔！</audio>
+<script>
+var audioObject = document.querySelector('#music')
+</script>
+```
+
+**方法2**
+```javascript
+var audioObject = new Audio('http://cloud.hunger-valley.com/music/玫瑰.mp3')
+```
+
+#### 2. audioObject.play()
+开始播放
+
+#### 3. audioObject.pause()
+暂停播放
+
+#### 4. audioObject.autoPlay
+设置或者获取自动播放状态
+
+```javascript
+audioObject.autoPlay = true  //设置为自动播放，下次更换 audioObject.src 后会自动播放音乐
+audioObject.autoPlay = false //设置不自动播放
+console.log(audioObject.autoPlay)
+```
+
+#### 5.audioObject.src
+设置或者获取音乐地址
+
+```javascript
+audioObject.src = "http://cloud.hunger-valley.com/music/ifyou.mp3"
+console.log(audioObject.src)
+```
+#### 6. audioObject.volume
+设置或者获取音量，最大值为1，0为静音
+
+```javascript
+audioObject.volume = 0.5
+audioObject.volume = 1
+console.log(audioObject.volume)
+```
+#### 7. audioObject.loop
+设置或者获取循环状态
+
+```javascript
+audioObject.loop = true
+console.log(audioObject.loop)
+```
+
+#### 8. audioObject.duration
+获取音乐长度，单位为秒
+
+```javascript
+console.log(audioObject.duration)
+```
+
+#### 9. audioObject.currentTime
+设置或者获取播放时间
+
+```javascript
+console.log(audioObject.currentTime)
+```
+#### 10. audioObject.ended
+判断音乐是否播放完毕，只读属性
+
+### 事件
+#### 1. `playing`
+当音乐开始播放，暂停后重新开始播放，设置currentTime后开始播放时触发
+
+```javascript
+audioObject.addEventListener('playing', function(){
+  console.log('playing')
+})
+```
+
+#### 2. `pause`
+当音乐暂停时和结束时触发
+
+```javascript
+audioObject.addEventListener('pause', function(){
+  console.log('pause')
+})
+```
+#### 3. `ended`
+当音乐结束时触发
+
+```javascript
+audioObject.addEventListener('ended', function(){
+  console.log('ended')
+})
+```
+#### 3. `timeupdate `
+当currentTime更新时会触发timeupdate事件,这个事件的触发频率由系统决定，但是会保证每秒触发4-66次（前提是每次事件处理不会超过250ms.
+
+```javascript
+
+//如下代码设置 每1秒左右执行一次
+audioObject.shouldUpdate = true
+audioObject.ontimeupdate = function(){
+  var _this = this
+  if(_this.shouldUpdate) {
+     //do something
+     console.log('update')
+     _this.shouldUpdate = false
+    setTimeout(function(){
+      _this.shouldUpdate = true
+    }, 1000)
+  }
+}
+```
+#### 4. `volumechange`
+当音量改变时触发
+
+```javascript
+audioObject.onvolumechange = function(){
+  console.log('volumechange')
+}
+```
---
+++ musicPlayer/js/index.js
@@ -0,0 +1,125 @@
+/**
+ * Created by qiaohao on 2018/10/14.
+ */
+
+
+
+var musicList = []
+var currentIndex = 0
+var audio = new Audio()
+audio.autoplay = true
+var clock
+
+
+
+getMusicList(function(list){
+    musicList = list
+    loadMusic(currentIndex)
+    generateList(musicList)
+})
+
+function generateList(list){
+    list.forEach(function(value){
+        var li = document.createElement('li');
+        li.innerText = value.auther + '-' + value.title
+        $('.list').appendChild(li)
+    })
+}
+
+audio.onplay = function () {
+    clock = setInterval(function () {
+        $('.musicbox .progress-now').style.width =  (audio.currentTime / audio.duration) * 100 + '%'
+        var min = Math.floor(audio.currentTime / 60)
+        var sec = Math.floor(audio.currentTime % 60) + ''
+        sec = sec.length === 2 ? sec : '0' + sec
+        $('.musicbox .time').innerText = min + ':' + sec
+    },1000)
+    $('.list').children[currentIndex].classList.add('selected')
+
+}
+
+audio.onpause = function () {
+    clearInterval(clock)
+}
+
+audio.onended = function () {
+    currentIndex = ( musicList.length +  --currentIndex) % musicList.length
+    loadMusic(currentIndex)
+}
+
+$('.musicbox .play').onclick = function () {
+    if(audio.paused){
+        audio.play()
+        this.querySelector('.fa').classList.remove('fa-play')
+        this.querySelector('.fa').classList.add('fa-pause')
+    }else{
+        audio.pause()
+        this.querySelector('.fa').classList.remove('fa-pause')
+        this.querySelector('.fa').classList.add('fa-play')
+    }
+}
+
+$('.musicbox .forward').onclick = function () {
+    currentIndex = (++currentIndex) % musicList.length
+    loadMusic(currentIndex)
+}
+
+$('.musicbox .back').onclick = function () {
+    currentIndex = ( musicList.length +  --currentIndex) % musicList.length
+    loadMusic(currentIndex)
+}
+
+
+$('.musicbox .bar').onclick = function (e) {
+    console.log(e)
+    var percent = e.offsetX / parseInt(getComputedStyle(this).width)
+    audio.currentTime = audio.duration * percent
+}
+
+
+function getMusicList(callback){
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET','music.json',true)
+    xhr.onload = function () {
+        if((xhr.status >= 200 && xhr.status < 300) || xhr.status ===304){
+            callback(JSON.parse(this.responseText))
+        }else{
+            console.log('获取数据失败')
+        }
+    }
+    xhr.onerror = function () {
+        console.log('网络异常')
+    }
+    xhr.send()
+}
+
+
+function loadMusic(index){
+    var musicObj = musicList[index]
+    audio.src = musicObj.src
+    console.log(musicObj.src)
+    $('.musicbox .title').innerText = musicObj.title
+    $('.musicbox .auther').innerText = musicObj.auther
+    $('.cover').style.backgroundImage = 'url(' + musicObj.img + ')'
+
+}
+
+
+function $(selector){
+    return document.querySelector(selector)
+}
+
+$('.list').onclick = function(e){
+    if(e.target.nodeName.toLowerCase() == 'li'){
+        for(var i = 0 ; i < this.children.length ; i ++){
+            if(e.target == this.children[i]){
+                currentIndex = i
+                loadMusic(currentIndex)
+            }else{
+                this.children[i].classList.remove('selected')
+            }
+
+        }
+    }
+
+}
